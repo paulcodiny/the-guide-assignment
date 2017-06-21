cc37f88d3f702c0d507ea4d7654a3fff8caf635c
Pavlik <paulcodiny@gmail.com>
2017-06-21 14:33:55 +0200

    1-hour refactoring of the function. Work done: 1. Split one monolith function into several independent in sake of readability and testability 2. Use async/await and NodeJS 8 promisify to simplify code and get rid of nested callbacks 3. Regular expression for onboard was changed to startsWith String utility method. Btw. the RegEx itself was not right
    Work To Do:
    Method startContinuousIntegrationJob is incorrect now, have to change promise based implementation to async/await + change some logic (like get rid of done functions, remove unreachable internal conditions)

--- src/handle-hook.js
+++ src/handle-hook.js
@@ -1,145 +1,188 @@
-function handleHook(input, pushId, callback) {
-    const validateError = validateInput(input);
-    if (validateError) {
-        return callback(validateError);
+const { promisify } = require('util');
+
+function errorNotifier(errorText) {
+    return function (error) {
+        logger.info(errorText);
+
+        return Promise.reject(error);
     }
-    // Lets shorten variables we need multiple times
-    const projectCloneUrl = input.projectCloneUrl;
-    const branch = input.branch;
-    async.waterfall([
-        // Get the packageJson from gitlab
-        (done) => {
-            const gitlabApiInput = {
-                group: Project.getGroupByCloneUrl(projectCloneUrl),
-                project: Project.getProjectByCloneUrl(projectCloneUrl),
-                branch,
-                fileName: 'package.json',
-            };
-            gitlabApiClient.getFile(
-                gitlabApiInput,
-                errorCallback(`Package.json not found in ${branch} branch`, done)
-            );
-        },
-        // // Get the project, based on the name in the packageJson
-        (packageJson, done) => {
-            projectService.findProjectByID(packageJson.name,
-                errorCallback(
-                    `An error occured when retrieving ${packageJson.name}`,
-                    packageJson,
-                    done
-                )
-            );
-        },
-        // Check if build is required
-        (packageJson, project, done) => {
-            let onboard = false;
-            if (project.onboard_branch_prefix) {
-                const regex = new RegExp(`^${project.onboard_branch_prefix}\\w+`);
-                onboard = regex.test(branch);
+}
+
+async function getPackageJson({ projectCloneUrl, branch }) {
+    const gitlabApiInput = {
+        group: Project.getGroupByCloneUrl(projectCloneUrl),
+        project: Project.getProjectByCloneUrl(projectCloneUrl),
+        branch,
+        fileName: 'package.json',
+    };
+
+  return await promisify(gitlabApiClient.getFile)(gitlabApiInput)
+      .catch(errorNotifier(`Package.json not found in ${branch} branch`));
+}
+
+async function getProjectInfo(packageJson) {
+  return await promisify(projectService.findProjectByID)(packageJson.name)
+      .catch(errorNotifier(`An error occured when retrieving ${packageJson.name}`));
+}
+
+function getReleaseConfiguration(packageJson, project, { mergeRequest, targetBranch, branch }) {
+    let releaseConfiguration;
+    if (mergeRequest === true) {
+        releaseConfiguration = Project.getReleaseConfigurationForBranch(
+            targetBranch,
+            project.release_configuration
+        );
+        releaseConfiguration.release = false;
+    } else {
+        releaseConfiguration = Project.getReleaseConfigurationForBranch(
+            branch,
+            project.release_configuration
+        ) || Project.getReleaseConfigurationForBranch(
+            project.onboard_branch_prefix,
+            project.release_configuration
+        );
+    }
+
+    if (!releaseConfiguration) {
+        logger.info(`Not triggering build for ${branch} branch of ${packageJson.name} no runner job specified`);
+
+        throw new Error('Not triggering', 42);
+    }
+
+    return releaseConfiguration;
+}
+
+function isOnboard({ onboard_branch_prefix }, { branch }) {
+    return onboard_branch_prefix && branch.startsWith(onboard_branch_prefix + '\\');
+}
+
+async function getVersionInfo(packageJson, project, { branch, sha }) {
+    return await promisify(getVersionToBuild)(
+        packageJson,
+        project.id,
+        branch,
+        sha,
+    ).catch(errorNotifier(`Could not find a version for ${packageJson.name}`));
+}
+
+async function createBuild(
+    packageJson,
+    project,
+    releaseConfiguration,
+    version,
+    pushId,
+    { branch }
+) {
+    const build = new Build({
+        initiator_project_id: project.id,
+        project_id: project.id,
+        type: project.type,
+        branch,
+        version,
+        push_id: pushId,
+        visible_at_dashboard: releaseConfiguration.release,
+        onboard: isOnboard(project, { branch }),
+    });
+    build.setStatus('queued');
+
+    return await promisify(buildService.insertBuild)(build)
+        .catch(errorNotifier(`Could not save the build for ${packageJson.name}`));
+}
+
+async function startContinuousIntegrationJob(
+    packageJson,
+    project,
+    releaseConfiguration,
+    version,
+    build,
+    { projectCloneUrl, branch }
+) {
+    const buildId = build.generated_keys[0];
+
+    return promisify(gitlabApiClient.setCommitStatus)(buildId)
+        .then((errorSetCommit) => {
+            if (errorSetCommit) {
+                logger.info('could not update the status of the git commit.');
             }
-            let release_configuration;
-            if (input.mergeRequest === true) {
-                release_configuration = Project.getReleaseConfigurationForBranch(input.targetBranch, project.release_configuration);
-                release_configuration.release = false;
-            } else {
-                release_configuration = Project.getReleaseConfigurationForBranch(branch, project.release_configuration) || Project.getReleaseConfigurationForBranch(project.onboard_branch_prefix, project.release_configuration);
+
+            return promisify(buildServerClient.triggerJenkinsJob)({
+                project_type: project.type,
+                project,
+                project_clone_url: projectCloneUrl,
+                project_branch: branch,
+                project_version: version,
+                build_id: buildId,
+                onboard: isOnboard(project, { branch }),
+                releaseConfiguration,
+            });
+        })
+        .then((errorTriggerJenkinsJob, resultTriggerJenkinsJob) => {
+            if (!errorTriggerJenkinsJob && resultTriggerJenkinsJob === true) {
+                return done();
             }
-            if (!release_configuration) {
-                logger.info(`Not triggering build for ${branch} branch of ${packageJson.name} no runner job specified`);
-                return done('not triggering');
-            }
-            return done(null, packageJson, project, release_configuration, onboard);
-        },
-        // check versions
-        (packageJson, project, release_configuration, onboard, done) => {
-            getVersionToBuild(
-                packageJson,
+
+            const subject = `Could not trigger jenkins job for v${packageJson.version}`;
+            notificationService.insertNotificationForBuild(
+                subject,
+                errorTriggerJenkinsJob.toString(),
+                'error',
                 project.id,
                 branch,
-                input.sha,
-                errorCallback(
-                    `Could not find a version for ${packageJson.name}`,
-                    packageJson,
-                    project,
-                    release_configuration,
-                    onboard,
-                    done
-                )
+                buildId
             );
-        },
-        // Create and save the new build
-        (packageJson, project, release_configuration, onboard, version, done) => {
-            const build = new Build({
-                initiator_project_id: project.id,
-                project_id: project.id,
-                type: project.type,
+
+            return promisify(buildServiceService.updateBuildStatus)({
+                build_id: buildId,
+                status: 'error',
+                extra_information: errorTriggerJenkinsJob.toString(),
+                enddatetime: new Date(),
+            });
+        })
+        .then((errorUpdateBuild, resultUpdateBuild) => {
+            if (!errorUpdateBuild) {
+                return errorCallback(`Could not trigger jenkins job for ${packageJson.name}`, done)(errorTriggerJenkinsJob, resultTriggerJenkinsJob);
+            }
+
+            notificationService.insertNotificationForBuild(
+                subject,
+                errorUpdateBuild.toString(),
+                'error',
+                project.id,
                 branch,
-                version,
-                push_id: pushId,
-                visible_at_dashboard: release_configuration.release,
-                onboard,
-            });
-            build.setStatus('queued');
-            buildService.insertBuild(build, errorCallback(
-                `Could not save the build for ${packageJson.name}`,
-                packageJson,
-                project,
-                release_configuration,
-                onboard,
-                version,
-                done
-            ));
-        },
-        // Start the jenkins job
-        (packageJson, project, release_configuration, onboard, version, savedBuild, done) => {
-            const buildId = savedBuild.generated_keys[0];
-            gitlabApiClient.setCommitStatus(buildId, (errorSetCommit) => {
-                if (errorSetCommit) {
-                    logger.info('could not update the status of the git commit.');
-                }
-                buildServerClient.triggerJenkinsJob({
-                    project_type: project.type,
-                    project,
-                    project_clone_url: projectCloneUrl,
-                    project_branch: branch,
-                    project_version: version,
-                    build_id: buildId,
-                    onboard,
-                    release_configuration,
-                }, (errorTriggerJenkinsJob, resultTriggerJenkinsJob) => {
-                    if (!errorTriggerJenkinsJob && resultTriggerJenkinsJob === true) {
-                        return done();
-                    }
-                    const subject = `Could not trigger jenkins job for v${packageJson.version}`;
-                    notificationService.insertNotificationForBuild(
-                        subject,
-                        errorTriggerJenkinsJob.toString(),
-                        'error',
-                        project.id,
-                        branch,
-                        buildId
-                    );
-                    buildServiceService.updateBuildStatus({
-                        build_id: buildId,
-                        status: 'error',
-                        extra_information: errorTriggerJenkinsJob.toString(),
-                        enddatetime: new Date(),
-                    }, (errorUpdateBuild, resultUpdateBuild) => {
-                        if (!errorUpdateBuild) {
-                            return errorCallback(`Could not trigger jenkins job for ${packageJson.name}`, done)(errorTriggerJenkinsJob, resultTriggerJenkinsJob);
-                        }
-                        notificationService.insertNotificationForBuild(
-                            subject,
-                            errorUpdateBuild.toString(),
-                            'error',
-                            project.id,
-                            branch,
-                            buildId
-                        );
-                        return errorCallback(`Could not update build record with id: ${buildId}`, done)(errorUpdateBuild, resultUpdateBuild);
-                    });
-                });
-            });
-        }
-    ], callback);
+                buildId
+            );
+
+            return errorCallback(`Could not update build record with id: ${buildId}`, done)(errorUpdateBuild, resultUpdateBuild);
+        });
+}
+
+async function handleHookAsync(input, pushId) {
+    const validateError = validateInput(input);
+    if (validateError) {
+        throw new Error(validateError, 21);
+    }
+
+    // Get the packageJson from gitlab
+    const packageJson = await getPackageJson(input);
+
+    // Get the project, based on the name in the packageJson
+    const project = await getProjectInfo(packageJson);
+
+    // Check if build is required
+    const releaseConfiguration = getReleaseConfiguration(packageJson, project, input);
+
+    // Check versions
+    const version = await getVersionInfo(packageJson, project, input);
+
+    // Create and save the new build
+    const build = await createBuild(packageJson, project, releaseConfiguration, version, pushId, input);
+
+    // Start the jenkins job
+    return startContinuousIntegrationJob(packageJson, project, releaseConfiguration, version, build, input);
+}
+
+function handleHook(input, pushId, callback) {
+    handleHookAsync(input, pushId)
+        .then(callback)
+        .catch(callback);
 }
